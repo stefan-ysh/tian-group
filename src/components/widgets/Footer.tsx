d75f27d82aa4d0f7f00d7d3d4f459bf4eb8371e4
yshuai <58702199+stefan-ysh@users.noreply.github.com>
2025-01-03 16:54:58 +0800

    hide socials

--- src/components/widgets/Footer.tsx
+++ src/components/widgets/Footer.tsx
@@ -52,7 +52,7 @@ const Footer = () => {
             </div>
           ))}
         </div>
-        <div className="py-6 md:flex md:items-center md:justify-between md:py-8">
+        {/* <div className="py-6 md:flex md:items-center md:justify-between md:py-8">
           <ul className="mb-4 flex md:order-1 md:ml-4 rtl:md:ml-0 rtl:md:mr-4 md:mb-0">
             {socials.map(({ label, icon: Icon, href }, index) => (
               <li key={`item-social-${index}`}>
@@ -66,7 +66,7 @@ const Footer = () => {
               </li>
             ))}
           </ul>
-        </div>
+        </div> */}
         {footNote}
       </div>
     </footer>
