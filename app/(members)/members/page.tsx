d7a8a4608e8ee207934ec8da4496c9286a75d3d6
yshuai <58702199+stefan-ysh@users.noreply.github.com>
2025-01-14 14:52:30 +0800

    refa: padding of member list container

--- app/(members)/members/page.tsx
+++ app/(members)/members/page.tsx
@@ -15,7 +15,7 @@ export default async function Home({}) {
 
   return (
     <section className="mx-auto px-0 md:px-20 py-12 pt-0">
-      <div className="grid grid-cols-1 gap-6 p-4 md:p-0 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <div className="grid grid-cols-1 gap-6 p-4 pt-0 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
         {members.map(({ slug, name, avatar, position }: { slug: string; name: string; avatar: string, position: string }) => (
           <MemberItem key={slug} name={name} avatar={avatar} slug={slug} position={position} />
         ))}
