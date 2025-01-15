8c73e79d6cee1da2e4196c8bbe65a41c4e1515ad
yshuai <58702199+stefan-ysh@users.noreply.github.com>
2025-01-15 11:25:34 +0800

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -7,6 +7,43 @@ module.exports = {
   priority: 0.8,
   sitemapSize: 5000,
   generateRobotsTxt: true,
+  additionalPaths: async (config) => {
+    const result = []
+
+    // required value only
+    // result.push({ loc: '/additional-page-1' })
+
+    // all possible values
+    // result.push({
+    //   loc: '/additional-page-2',
+    //   changefreq: 'yearly',
+    //   priority: 0.7,
+    //   lastmod: new Date().toISOString(),
+
+    //   // acts only on '/additional-page-2'
+    //   alternateRefs: [
+    //     {
+    //       href: 'https://es.example.com',
+    //       hreflang: 'es',
+    //     },
+    //     {
+    //       href: 'https://fr.example.com',
+    //       hreflang: 'fr',
+    //     },
+    //   ],
+    // })
+
+    // using transformation from the current configuration
+    result.push(await config.transform(config, '/'))
+    result.push(await config.transform(config, '/research'))
+    result.push(await config.transform(config, '/publications'))
+    result.push(await config.transform(config, '/members'))
+    result.push(await config.transform(config, '/news'))
+    result.push(await config.transform(config, '/activities'))
+    result.push(await config.transform(config, '/joinus'))
+    result.push(await config.transform(config, '/contact'))
+    return result
+  },
   transform: async (config, path) => {
     let priority = config.priority;
     let changefreq = config.changefreq;
