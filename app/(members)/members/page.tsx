0f0f3897cf031c6b74211400e3db8fbb3194e974
yshuai <58702199+stefan-ysh@users.noreply.github.com>
2025-01-16 09:48:02 +0800

    fix: title description

--- app/(members)/members/page.tsx
+++ app/(members)/members/page.tsx
@@ -6,7 +6,7 @@ import { MemberItem } from '~/components/widgets/MemberItem';
 
 export const metadata: Metadata = {
   title: '组内成员 | 田甜科研小组',
-  description: '扬州大学化学学院，田甜的科研小组，主要研究方向包括环糊精、钙钛矿、太阳能电池等, 多项科研成果发布在Angew, JACS, JPC, NC, Wiley, Nature, Science, Advanced Materials, Advanced Functional Materials, ACS Nano, ACS Catalysis, ACS Energy Letters, ACS Energy & Fuels, ACS Sustainable Chemistry & Engineering, Journal of Materials Chemistry A',
+  description: '扬州大学化学学院田甜科研小组，专注环糊精、钙钛矿、太阳能电池研究，成果发表于Angew、JACS、JPC、NC、Wiley、Nature、Science、Advanced Materials等众多权威期刊。',
   keywords: '田甜, 扬州大学, 化学学院, 科研实验室, 环糊精, 钙钛矿, 太阳能电池, 教授, Angew, JACS, JPC, NC, Wiley, Nature, Science, Advanced Materials, Advanced Functional Materials, ACS Nano, ACS Catalysis, ACS Energy Letters, ACS Energy & Fuels, ACS Sustainable Chemistry & Engineering, Journal of Materials Chemistry A',
   openGraph: {
     title: '组内成员 | 田甜科研小组',
