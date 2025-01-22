ca44382fe1caeb71aac741baf3c43cb858026863
yshuai <58702199+stefan-ysh@users.noreply.github.com>
2025-01-22 21:36:27 +0800

    opt

--- app/(activities)/activities/page.tsx
+++ app/(activities)/activities/page.tsx
@@ -7,7 +7,7 @@ import { ActivityItem } from '~/components/widgets/ActivityItem';
 
 export const metadata: Metadata = {
   title: '最新活动 | 田甜科研小组',
-  description: '扬州大学化学学院田甜科研小组，专注环糊精、钙钛矿、太阳能电池研究，成果发表于Angew、JACS、JPC、NC、Wiley、Nature、Science、Advanced Materials等众多权威期刊。',
+  description: '扬州大学化学学院庞欢课题组-田甜科研小组，专注环糊精、钙钛矿、太阳能电池研究，成果发表于Angew、JACS、JPC、NC、Wiley、Nature、Science、Advanced Materials等众多权威期刊。',
   keywords: '田甜, 扬州大学, 化学学院, 科研实验室, 环糊精, 钙钛矿, 太阳能电池, 教授, Angew, JACS, JPC, NC, Wiley, Nature, Science, Advanced Materials, Advanced Functional Materials, ACS Nano, ACS Catalysis, ACS Energy Letters, ACS Energy & Fuels, ACS Sustainable Chemistry & Engineering, Journal of Materials Chemistry A',
   openGraph: {
     title: '最新活动 | 田甜科研小组',
