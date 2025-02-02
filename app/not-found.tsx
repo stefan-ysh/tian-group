b5e87ed9776c1ccb2d6a5b70ea5b4ca7bb5f62e2
yshuai <58702199+stefan-ysh@users.noreply.github.com>
2025-02-02 09:58:58 +0800

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Not Fou2nd</h1>
+    <div className="w-full text-center">
+      <h1>404</h1>
+      <h1>This page is not found</h1>
     </div>
   );
 }
