593b7286bc2204b3434b750f4d92842870ac45c1
yshuai <58702199+stefan-ysh@users.noreply.github.com>
2025-02-10 11:21:50 +0800

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,23 +1,48 @@
+import { Button, Card, CardBody, Image, Slider } from '@heroui/react';
+import { Mail, School } from 'lucide-react';
 import { Metadata } from 'next';
+import { useTranslations } from 'next-intl';
+import NextImage from 'next/image';
+import React from 'react';
 
 export const metadata: Metadata = {
-  title: '联系我们 | 田甜科研小组',
-  description: '扬州大学化学学院庞欢课题组-田甜科研小组，专注环糊精、钙钛矿、太阳能电池研究，成果发表于Angew、JACS、JPC、NC、Wiley、Nature、Science、Advanced Materials等众多权威期刊。, Journal of Materials Chemistry B, Journal of Materials Chemistry C, Journal of Materials Chemistry D, Journal of Materials Chemistry E, Journal of Materials Chemistry F, Journal of Materials Chemistry G, Journal of Materials Chemistry H, Journal of Materials Chemistry I, Journal of Materials Chemistry',
-  keywords: '田甜, 扬州大学, 化学学院, 科研实验室, 环糊精, 钙钛矿, 太阳能电池, 教授, Angew, JACS, JPC, NC, Wiley, Nature, Science, Advanced Materials, Advanced Functional Materials, ACS Nano, ACS Catalysis, ACS Energy Letters, ACS Energy & Fuels, ACS Sustainable Chemistry & Engineering, Journal of Materials Chemistry A, Journal of Materials Chemistry B, Journal of Materials Chemistry C, Journal of Materials Chemistry D, Journal of Materials Chemistry E, Journal of Materials Chemistry F, Journal of Materials Chemistry G, Journal of Materials Chemistry H, Journal of Materials Chemistry I, Journal of Materials Chemistry',
+  title: '田甜科研小组 | 扬州大学化学学院',
+  description:
+    '扬州大学化学学院庞欢课题组-田甜科研小组，专注环糊精、钙钛矿、太阳能电池研究，成果发表于Angew、JACS、JPC、NC、Wiley、Nature、Science、Advanced Materials等众多权威期刊。',
+  keywords:
+    '田甜, 扬州大学, 化学学院, 科研实验室, 环糊精, 钙钛矿, 太阳能电池, 教授, Angew, JACS, JPC, NC, Wiley, Nature, Science, Advanced Materials, Advanced Functional Materials, ACS Nano, ACS Catalysis, ACS Energy Letters, ACS Energy & Fuels, ACS Sustainable Chemistry & Engineering, Journal of Materials Chemistry A',
   openGraph: {
-    title: '联系我们 | 田甜科研小组',
-    description: '扬州大学化学学院庞欢课题组-田甜科研小组，专注环糊精、钙钛矿、太阳能电池研究，成果发表于Angew、JACS、JPC、NC、Wiley、Nature、Science、Advanced Materials等众多权威期刊。, Journal of Materials Chemistry B, Journal of Materials Chemistry C, Journal of Materials Chemistry D, Journal of Materials Chemistry E, Journal of Materials Chemistry F, Journal of Materials Chemistry G, Journal of Materials Chemistry H, Journal of Materials Chemistry I, Journal of Materials Chemistry'
-  }
-}
+    title: '田甜科研小组 | 扬州大学化学学院',
+    description:
+      '扬州大学化学学院庞欢课题组-田甜科研小组，专注环糊精、钙钛矿、太阳能电池研究，成果发表于Angew、JACS、JPC、NC、Wiley、Nature、Science、Advanced Materials等众多权威期刊。',
+  },
+};
 
-export default async function ContactWrapper({}) {
+export default function Contact() {
+  const t = useTranslations('HomePage');
   return (
-    <section className="mx-auto max-w-3xl px-6 py-12 sm:px-6 sm:py-16 lg:py-20 pt-0">
-      <header>
-        <h1 className="leading-tighter font-heading mb-8 text-center text-4xl font-bold tracking-tighter md:mb-16 md:text-5xl">
-          Contact in Progress
-        </h1>
-      </header>
-    </section>
+    <Card
+      isBlurred
+      className="border-none bg-background/60 dark:bg-default-100/50 max-w-[610px] mx-auto my-10"
+      shadow="sm"
+    >
+      <CardBody>
+        <div className="flex flex-col z-50">
+          <div className="w-full text-sm">
+            <div className="flex items-start md:items-center flex-col md:flex-row gap-2 my-10">
+              <Mail size={16} />
+              <a href="mailto:tiant91@yzu.edu.cn" className="whitespace-nowrap">
+                tiant91@yzu.edu.cn
+              </a>
+            </div>
+            <div className="flex items-start md:items-center flex-col md:flex-row gap-2 my-10">
+              <School size={16} />
+              <span>{t('Address')}</span>
+            </div>
+          </div>
+        </div>
+        {/* <Globe /> */}
+      </CardBody>
+    </Card>
   );
 }
