3c917b9bf3a2657847ba6613570516b3ec8bf6c4
stefan-ysh <58702199+stefan-ysh@users.noreply.github.com>
2025-10-13 21:59:58 +0800

    test:ppt

---
+++ public/html/ppt/assets/md-xhUeus0o.js
@@ -0,0 +1,11 @@
+import{aL as d,b as c,o as n,w as m,g as e,i as u,ah as o,f as r,ad as s,v as p,x as f,T as a}from"./modules/vue-D2ZY8cbv.js";import{I as y}from"./default-B2TeuL9P.js";import{u as _,f as b}from"./slidev/context-NXbt81i2.js";import"./index-B1CRNN48.js";import"./modules/shiki-x6-3M-An.js";const g="/26-2.jpeg",w={src:g,absolute:"",inset:"0",initial:{x:0},enter:{x:0},"click-1":{x:0},transition:"ease-out",duration:"1300"},k={absolute:"",initial:{x:-36,y:500},enter:{x:-36,y:500},"click-1":{x:-36,y:390},w:"68",bg:"#79848960",p:"5","rounded-lg":"",flex:"~ items-center justify-between",style:{transform:"skew(-12deg)"},"text-white":""},v={absolute:"",initial:{x:240,y:500},enter:{x:240,y:500},"click-2":{x:240,y:390},w:"91",bg:"#79848960",style:{transform:"skew(-12deg)"},p:"5","rounded-lg":"",flex:"~ items-center justify-between","text-white":""},j={absolute:"",initial:{x:610,y:500},enter:{x:610,y:500},"click-3":{x:610,y:390},w:"88",bg:"#79848960",p:"5","rounded-lg":"",flex:"~ items-center justify-between",style:{transform:"skew(-12deg)"},"text-white":""},D={__name:"slides.md__slidev_26",setup(h){const{$clicksContext:l,$frontmatter:x}=_();return l.setup(),(B,t)=>{const i=d("motion");return n(),c(y,p(f(a(b)(a(x),25))),{default:m(()=>[t[3]||(t[3]=e("h3",{"text-center":""},"柔性大面积多色长余辉纤维膜在建筑修饰中的应用",-1)),u(` <img
+src="/26-1.jpeg"
+absolute
+inset="0"
+v-motion
+:initial="{ x: 0  }"
+:enter="{ x: 0 }"
+:click-1="{ x: 500 }"
+transition="ease-out"
+duration="1300"
+/> `),o(e("img",w,null,512),[[i]]),o((n(),r("div",k,[...t[0]||(t[0]=[e("span",{class:"text-xs"},[s(" 户外发光步道 "),e("br"),s(" 在广场、公园铺设，营造梦幻互动夜景及安全指引功能，有效吸引人流，提升商业价值 ")],-1)])])),[[i]]),o((n(),r("div",v,[...t[1]||(t[1]=[e("span",{class:"text-xs"},[s(" 室内墙面装饰 "),e("br"),s(" 贴合墙面与天花板，为KTV、展厅等空间营造如极光般绚烂的沉浸式氛围，提升空间的艺术感与科技感 ")],-1)])])),[[i]]),o((n(),r("div",j,[...t[2]||(t[2]=[e("span",{class:"text-xs"},[s(" 发光艺术装置 "),e("br"),s(" 结合花坛、雕塑等打造夜晚的视觉焦点，创造“网红打卡点”，吸引游客拍照传播，迅速提升项目人气 ")],-1)])])),[[i]])]),_:1},16)}}};export{D as default};
