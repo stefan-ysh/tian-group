08997f3536817f617e9fed2c2af9079752575eaf
stefan-ysh <58702199+stefan-ysh@users.noreply.github.com>
2026-05-07 21:58:56 +0800

    Update MemberClientPage.jsx

--- app/(members)/members/[slug]/MemberClientPage.jsx
+++ app/(members)/members/[slug]/MemberClientPage.jsx
@@ -235,7 +235,7 @@ export function MemberClientPage({ slug, initialMember = null }) {
 
             {member.content && (
               <div
-                className="prose-md prose-headings:font-heading prose-headings:leading-tighter container mx-auto prose-lg  mt-12 px-6 prose-headings:font-bold prose-headings:tracking-tighter prose-a:text-primary-600 prose-img:rounded-md prose-img:shadow-lg dark:prose-invert dark:prose-headings:text-slate-300 dark:prose-a:text-primary-400 sm:px-6 lg:prose-xl leading-relaxed"
+                className="prose-md prose-headings:font-heading prose-headings:leading-tighter container mx-auto prose-lg  mt-12 px-0 prose-headings:font-bold prose-headings:tracking-tighter prose-a:text-primary-600 prose-img:rounded-md prose-img:shadow-lg dark:prose-invert dark:prose-headings:text-slate-300 dark:prose-a:text-primary-400 sm:px-6 lg:prose-xl leading-relaxed"
                 dangerouslySetInnerHTML={{
                   __html: md({
                     html: true,
